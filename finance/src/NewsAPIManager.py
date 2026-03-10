6e61ee037fa0b20125ce6373ba0eb74654e36c44
Emma Talley <mcrib40@gmail.com>
2026-03-10 15:55:54 -0400

    Delete finance directory

--- finance/src/NewsAPIManager.py
+++
@@ -1,8 +0,0 @@
-
-
-class NewsAPIManager(DbManger):
-    """
-    Handles requests to newsAPI. Used to populate the homepage newsfeed alongside news stories
-    gatherd in '/research'.
-    """
-    
