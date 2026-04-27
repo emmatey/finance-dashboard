c3e116df4975fa3bcfac884b43fb08f2fba6086b
Emma Talley <emma.talley01@gmail.com>
2026-04-27 18:11:00 -0400

    updated starting cash

--- src/test_requests.py
+++ src/test_requests.py
@@ -3,8 +3,8 @@ import requests
 url = "http://localhost:5000"
 s = requests.Session()
 
-#response = s.post(f'{url}/auth/register', 
-#    json={"username": "emma", "password": "123Pass"})
+response = s.post(f'{url}/auth/register', 
+    json={"username": "emma", "password": "123Pass"})
 
 #response = s.post(f'{url}/auth/login', 
 #                  json={"username": "emma", "password": "123Pass"})
