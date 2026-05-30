5b7dc973bcbeba5e00263a2690a0d4daa843f670
Emma Talley <emma.talley01@gmail.com>
2026-05-30 13:01:58 -0700

    remove component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,28 @@
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
-import LogInCard from '../components/LogInCard.jsx';
+import LogIn from '../components/LogIn.jsx';
+import { useState } from "react"
 
 export default function Login() {
-    
+    const[username, setUsername] = useState("emma");
+    const[password, setPassword] = useState("123");
+
+    async function login() {
+
+        
+        };
     return (
         <>
-        <Header></Header>
-        <LogInCard></LogInCard>
-        <Footer></Footer>
+        <div className="card">
+        <form>
+            <div className="input-group">
+                <input type="text" className="form-control"></input>
+                <input type="text" className="form-control"></input>
+                <button type='submit' className="btn-primary" onClick={login}>Log In!</button>
+            </div>
+        </form>
+        </div>
         </>
     )
 }
+
