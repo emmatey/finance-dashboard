1c48b7bbe23d49786928e2b13f5603f660057726
Emma Talley <emma.talley01@gmail.com>
2026-05-30 18:44:43 -0700

    feat: enhance login functionality with error handling and add Change and Register components

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -5,12 +5,19 @@ import { logIn } from "../../scripts/backend-fetch";
 export default function Login() {
     const[username, setUsername] = useState("");
     const[password, setPassword] = useState("");
+    // used to trigger elements which are in response to user entering incorrect login info.
+    const[wrong, setwrong] = useState(false);
 
     async function handleLogIn() {
         const status = await logIn(username, password);
 
-        console.log(status);
+        if (status == false) {
+            setwrong(true)
+        };
     }
+
+    const forgotPwStr = "Your username and password combination is unknown.";
+    const registerStr = "Register for a new account?";
     return (
         <>
         <div>
@@ -18,9 +25,11 @@ export default function Login() {
                 <h2>Log in!</h2>
                 <label htmlFor="username"> Username </label>
                 <input type="text" id="username" onChange={(e) => {setUsername(e.target.value)}}/>
+                {wrong ? <small><link to='/auth&mode=change'>{forgotPwStr}</link></small> : null}
 
                 <label htmlFor="password"> Password </label>
                 <input type="password" id="password" onChange={(e) => {setPassword(e.target.value)}}/>
+                {wrong ? <small><link to='/auth&mode=register'>{registerStr}</link></small> : null}
 
                 <button type="button" onClick={handleLogIn}> Log In </button>
             </form>
---
+++ frontend/src/components/auth/Register.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
+
+export default function Register() {
+    return (
+        <>
+            <Header />
+            <p>Register for a new account.</p>
+            <Footer />
+        </>
+    )
+}
