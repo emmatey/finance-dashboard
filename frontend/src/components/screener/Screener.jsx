3c8125a97a1d70d771fd1323c21020fda5e8f0a4
Emma Talley <emma.talley01@gmail.com>
2026-06-02 08:56:14 -0700

    feat: implement fetchScreenerData function with error handling for API response

--- frontend/src/components/screener/Screener.jsx
+++ frontend/src/components/screener/Screener.jsx
@@ -2,7 +2,26 @@ import Header from "../Header";
 import Footer from "../Footer";
 
 export default function Screener() {
-    
+    async function fetchScreenerData() {
+        try {
+            const url = "/api/screeners";
+            const response = fetch(url, {
+                method: "POST"  
+            });
+
+            const responseBody = await response.json();
+            // 500 or 200
+            if (response.status === 500) {
+               console.error(responseBody);
+               throw new Error(`Server responded with status: ${response.status}`); 
+            }
+
+
+        } catch (error) {
+            console.error(error.message);
+            return null;
+        };
+    }
 
 
     return (
