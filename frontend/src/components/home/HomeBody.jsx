ab8fff9c8d9b19fb6437217f5213795d8846eae4
emmatey <mcrib40@gmail.com>
2026-06-11 16:17:28 -0700

    partially complete tradeShard function

--- frontend/src/components/home/HomeBody.jsx
+++ frontend/src/components/home/HomeBody.jsx
@@ -2,6 +2,7 @@ import Header from '../Header.jsx'
 import Footer from '../Footer.jsx'
 import ScreenerShard from '../shards/screener/ScreenerShard.jsx'
 import TransactionHistoryShard from '../shards/transactionHistory/TransactionHistoryShard.jsx'
+import TradeShard from '../shards/trade/TradeShard.jsx'
 
 export default function HomeBody({ username }) {
     return (
@@ -12,6 +13,7 @@ export default function HomeBody({ username }) {
                 <h1>Hi {username || null}, Welcome to finance-dashboard, you ARE logged in!</h1>
             </div>
             <TransactionHistoryShard username={username} />
+            <TradeShard />
         </div>
         <Footer />
         </>
--- frontend/src/components/shards/trade/TradeShard.jsx
+++ frontend/src/components/shards/trade/TradeShard.jsx
@@ -3,78 +3,92 @@ import { parseResponse } from '../../../scripts/utils.js'
 import '../../../styles/utilities.css'
 import '../../../styles/colors.css'
 
-export default function TradeShard( {queryProp} ) {
+export default function TradeShard({ queryProp }) {
     const safeQueryProp = String(queryProp ?? "").trim();
-    const [query, setQuery] = useState("");
     const [loading, setLoading] = useState(false);
     const [tickerInfoJson, setTickerInfoJson] = useState(null);
-    const timeoutRef = useRef(null);
     // GET Returns data to populate the order preview screen.
     // POST Executes a buy or sell transaction.
 
     // Preview
-        // 1. request to trade url to get data on selected ticker
-            // Ticker will come in from two places, as a prop, or as a value of an internal el
-            // check for prop first, if not check input element next.
-        // 2. Start a timer on the age of the data. 60s?
-            // 2a. If the data gets older than that, refresh it. The state shown to the user will be
-            // the price of the order in the ledger. 
-    
-    // Transact
-        // 1. Callback
+    // 1. request to trade url to get data on selected ticker
+    // Ticker will come in from two places, as a prop, or as a value of an internal el
+    // check for prop first, if not check input element next.
+    // 2. Start a timer on the age of the data. 60s?
+    // 2a. If the data gets older than that, refresh it. The state shown to the user will be
+    // the price of the order in the ledger. 
 
-    async function getTrade( getQuery ) {
+    // Transact
+    // 1. Callback
+    async function runTask(func, param, interval) {
+        let isRunning = true;
+        // edit this to use a useEffect with a dependency on the query that calls the cleanup callback on return
+        // or use a use ref to store the callback which stops the loop
+        async function run() {
+            if (isRunning) {
+                await func(param);
+                setTimeout(() => (
+                    run()
+                ), interval)
+            }
+        }
+
+        run();
+
+        return () => (isRunning = false)
+    }
+
+    async function getTrade(query) {
         // Makes a GET request to the '/api/trade' route.
         try {
-            const tickerInfoResponse = await fetch(`/api/trade?ticker=${getQuery}`);
-            const tickerInfoJson = setTickerInfoJson((await parseResponse(tickerInfoResponse)?.data || []));
+            setLoading(true);
+            const tickerInfoResponse = await fetch(`/api/trade?ticker=${query}`);
+            const tickerJson = await parseResponse(tickerInfoResponse) || {}
+            setTickerInfoJson(tickerJson);
+            setLoading(false);
         } catch (error) {
             setLoading(false);
-            setTickerInfoJson([]);
+            setTickerInfoJson({});
             console.error(error);
         }
     }
 
     function postTrade() {
         // Makes a POST request to the '/api/trade' route.
-        
+
     }
 
-    function handleSearchSubmit() {
-        setQuery(String(event.target.closest('input').value).trim())
-        getTrade();
-        timeoutRef.current = setTimeout(() => {
-
-        }, 60000)
+    async function handleSearchSubmit(event) {
+        event.preventDefault();
+        // change this to query the event.queryselector
+        const query = String(document.getElementsByName('searchInput')[0].value).trim();
+        return runTask(getTrade, query, 60000);
     }
 
-    function handleTransactSubmit() {
-        
+    function handleTransactSubmit(event) {
+        event.preventDefault();
     }
-
-    // Refreshes price data on a timer.
-    useEffect(() => {
-
-        return(<></>)
-    }, [query])
-
-    // Clears timer on dismount.
-    useEffect(() => {
-        return (() => clearTimeout(timeoutRef.current));
-    }, []);
-
+    
     return (
         <div className='card'>
             <form name='tradeSearchForm' onSubmit={handleSearchSubmit} >
-                <input type='text'/>
+                <input name='searchInput' type='text' />
                 <button type='submit'> Search </button>
             </form>
 
             {
-            loading ? <h4> Loading... </h4> :
-            <ul>
-                {}
-            </ul>
+                loading ? <h4> Loading... </h4> :
+                    <ul>
+                        {
+                            !tickerInfoJson
+                                ?
+                                <li>No info found...</li>
+                                :
+                                Object.entries(tickerInfoJson).map(([k, v]) => (
+                                    <li key={k}>{k}: {String(v)}</li>
+                                ))
+                        }
+                    </ul>
             }
 
             <form name='tradeTransactForm' onSubmit={handleTransactSubmit}>
@@ -83,7 +97,7 @@ export default function TradeShard( {queryProp} ) {
                     <option value='sell'>Sell</option>
                 </select>
                 <div>
-                    <input type='number' name='qty' placeholder='Qty' min='0.1' step='0.1' />
+                    <input type='number' name='qtyInput' placeholder='Qty' min='0.1' step='0.1' />
                     <select name='qtyUnit'>
                         <option value='shares'>Shares</option>
                         <option value='dollars'>Dollars</option>
