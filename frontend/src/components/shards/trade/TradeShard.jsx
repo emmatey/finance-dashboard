ca8a7eedad6196757fe2712c5ba33f0473561b52
emmatey <mcrib40@gmail.com>
2026-06-12 19:02:00 -0700

    got hover multicolor working for some reason

--- frontend/src/components/shards/trade/TradeShard.jsx
+++ frontend/src/components/shards/trade/TradeShard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { parseResponse } from '../../../scripts/utils.js'
+import { getRandomIntInclusive } from '../../../scripts/utils.js'
 import '../../../styles/utilities.css'
 import '../../../styles/colors.css'
 
@@ -14,6 +15,11 @@ export default function TradeShard({ queryProp }) {
 
     async function getTrade() {
         // Makes a GET request to the '/api/trade' route.
+        if (!currentQuery) {
+            console.warn("no query provided on getTrade call.")
+            return
+        };
+
         try {
             setLoading(true);
             const tickerInfoResponse = await fetch(`/api/trade?ticker=${encodeURIComponent(currentQuery)}`);
@@ -66,21 +72,42 @@ export default function TradeShard({ queryProp }) {
         event.preventDefault();
     }
 
+    function handleSuggestionMouseOver(event) {
+        const el = event.target;
+        const val = getRandomIntInclusive(1, 4);
+        if (val === 1) {
+            el.style.background = 'var(--blue-3)';
+        } else if (val === 2) {
+            el.style.background = 'var(--yellow-4)';
+        } else if (val === 3) {
+            el.style.background = 'var(--red-3)';
+        } else if (val === 4) {
+            el.style.background = 'var(--green-3)';
+        } else {
+            throw new Error("invalid 'val' var in header.jsx");
+        };
+    }
+
+    function handleSuggestionMouseLeave(event) {
+        const el = event.target;
+        el.style.background = 'var(--color-surface)';
+    }
+
     // Updates data about current company every 60 seconds.
-    //useEffect(() => {
-    //   let timerId = null; 
-    //
-    //   async function tick() {
-    //        await getTrade();
-    //        timerId = setTimeout(() => {
-    //            tick();
-    //        }, 60000)
-    //   }
-    //
-    //   tick();
-    //
-    //   return () => {clearTimeout(timerId)}
-    //}, [currentQuery]);
+    useEffect(() => {
+       let timerId = null; 
+    
+       async function tick() {
+            await getTrade();
+            timerId = setTimeout(() => {
+                tick();
+            }, 60000)
+       }
+    
+       tick();
+    
+       return () => {clearTimeout(timerId)}
+    }, [currentQuery]);
 
     return (
         <div className='card'>
@@ -100,11 +127,14 @@ export default function TradeShard({ queryProp }) {
                             <ul style={{ position: 'absolute', top: '100%', left: 0, width: '100%', padding: 0, margin: 0 }}>
                                 {dataList.map((arr) => (
                                     <li
+                                        // arr = [name, ticker]
                                         key={arr[1]}
                                         className='card'
                                         onMouseDown={() => handleSuggestionSelect(arr[1])}
+                                        onMouseOver={handleSuggestionMouseOver}
+                                        onMouseLeave={handleSuggestionMouseLeave}
                                     >
-                                        {`${arr[1]} - ${arr[0]}`}
+                                        {`${arr[1] === "null" ? null : arr[1]} - ${arr[0] === "null" ? null : arr[0]}`}
                                     </li>
                                 ))}
                             </ul>
