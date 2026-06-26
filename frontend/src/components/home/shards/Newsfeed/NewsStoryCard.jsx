65f23afe651a77423641f0beb5fa0dae95b3430c
emmatey <mcrib40@gmail.com>
2026-06-26 10:05:22 -0700

    Added skeleton component from shadcn. Laid out news feed shard configuration.

---
+++ frontend/src/components/home/shards/Newsfeed/NewsStoryCard.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+
+export default function NewsStoryCard() {
+    /*
+        data: [{
+            uuid: str,
+            title: str,
+            link: str,
+            publisher: str,
+            thumbnail: str,
+            providerPublishTime: int
+        }]
+    */
+    /*
+        This card will be a horizontal rectangle that contains the thumbnail on the left
+        a clickable title in the body
+        the publisher below, and the date on the second line as well. 
+
+        This card needs the state for one story. However the parent should control the visibility and
+        the lifecycle.
+    */
+    return (
+        <>
+        </>
+    )
+}
--- frontend/src/components/home/shards/Newsfeed/NewsfeedShard.jsx
+++ frontend/src/components/home/shards/Newsfeed/NewsfeedShard.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import useNewsfeed from "./useNewsfeed";
+import NewsStoryCard from "./NewsStoryCard";
 
 export default function NewsfeedShard() {
-    const { loading, data, error, responseCode } = useNewsfeed();
+    /*
+        newsData: [{
+            uuid: str,
+            title: str,
+            link: str,
+            publisher: str,
+            thumbnail: str,
+            providerPublishTime: int
+        }]
+    */
+   const [newsData, setNewsData] = useState(null);
+   const { loading, data, error, responseCode } = useNewsfeed();
+   if (loading) return <div>Loading...</div>;
+   if (error) return <p>{`${error} ${responseCode}`}</p>;
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <p>{error}</p>;
 
+    /* This component calls the use newsfeed hook and owns the state
+    for that transaction. It also controls the visibility and lifecycle
+    of the child NewsStoryCard components.
+]
+    It should contain the html for the card and the scrollable frame 
+    that the news story cards sit within. 
+    */
     return (
         <div>
         </div>
