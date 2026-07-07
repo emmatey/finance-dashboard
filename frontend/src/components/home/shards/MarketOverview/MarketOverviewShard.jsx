d4f60b2d14bba5d5f73d464ed5e61a0e00817492
emmatey <mcrib40@gmail.com>
2026-07-07 16:48:55 -0700

    got basis of market overview working.

--- frontend/src/components/home/shards/MarketOverview/MarketOverviewShard.jsx
+++ frontend/src/components/home/shards/MarketOverview/MarketOverviewShard.jsx
@@ -5,12 +5,13 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 import { Spinner } from '@/components/ui/spinner';
 import getDisplayName from './overviewRegions.js';
+import TickerCard from './TickerCard.jsx';
 
 
 function sortByRegionKey(data) {
     const regionMap = new Map();
     for (const obj of data) {
-        if (typeof(obj?.region) === 'string') {
+        if (typeof (obj?.region) === 'string') {
             const displayName = getDisplayName(obj.region);
             if (regionMap.has(displayName)) {
                 regionMap.get(displayName).push(obj);
@@ -18,37 +19,58 @@ function sortByRegionKey(data) {
                 regionMap.set(displayName, [obj])
             };
         } else {
-            console.warn(`Type of obj.region not str ${typeof(obj?.region)}`);
+            console.warn(`Type of obj.region not str ${typeof (obj?.region)}`);
             continue;
         };
     }
-    if (regionMap) return regionMap;
-    return flase;
+    return regionMap;
 };
 
 export default function MarketOverviewShard() {
     const { loading, data, error } = useMarketOverview();
-    const regionMap = data ? sortByRegionKey(data) : {};
-
-    if (error) {
-        return (<p className="text-sm text-destructive">{error}</p>)
+    const regionMap = data ? sortByRegionKey(data) : new Map();
+    
+    const filteredMap = new Map();
+    const blackList = ['USA', 'Commodities'];
+    for (const [displayName, data] of regionMap.entries()) {
+        if (!blackList.includes(displayName)) {
+            filteredMap.set(displayName, data)
+        }
     };
+    let filteredMapTuples = [];
+    for (const[displayName, data] of filteredMap.entries()) {
+        filteredMapTuples.push([displayName, data]);
+    }
 
-    if (loading || regionMap.length === 0) {
-        return (
-            <Card>
-                <CardHeader>
-                    <CardTitle>Market Overview</CardTitle>
-                </CardHeader>
-                <CardContent className="flex items-center justify-center py-12">
-                    <Spinner className="size-6" />
-                </CardContent>
-            </Card>
-        );
-    };
-    console.log(regionMap);
     return (
         <Card>
+            <CardHeader>
+                <CardTitle>Market Overview</CardTitle>
+            </CardHeader>
+            <CardContent>
+
+                {loading &&
+                    <Spinner className="size-6" />
+                }
+
+                {error &&
+                    <p className="text-sm text-destructive">{error}</p>
+                }
+    
+                {regionMap.size > 0 && <MarketRegionCard name={"USA"} tickers={regionMap.get("USA")} />}
+
+                {filteredMapTuples.length > 0 && 
+                    <div>
+                        {
+                        filteredMapTuples.map(
+                            ([displayName, data], idx) => (data.map((tickerData) => (<TickerCard key={tickerData.ticker} ticker={tickerData} title={displayName} />))))
+                        }
+                    </div>
+                }
+                <Separator />
+                {regionMap.size > 0 && <MarketRegionCard name={"Commodities"} tickers={regionMap.get("Commodities")} />}
+
+            </CardContent>
         </Card>
     );
 }
--- frontend/src/components/home/shards/MarketOverview/TickerCard.jsx
+++ frontend/src/components/home/shards/MarketOverview/TickerCard.jsx
@@ -1,15 +1,20 @@
 import { formatCurrencyUSD } from '@/scripts/utils';
 import { Card, CardContent } from '@/components/ui/card';
 
-export default function TickerCard({ ticker }) {
+export default function TickerCard({ ticker, title=null }) {
     const { ticker: symbol, company_name, current_price, pct_change } = ticker;
     const isPositive = pct_change >= 0;
-
+    //company_name: "Invesco QQQ Trust"
+    //​​current_price: 709.43
+    //​​pct_change: -1.85
+    //prev_close: 722.82
+    //region: "USA Nasdaq"​​
+    //ticker: "QQQ"
     return (
         <Card size="sm" className="w-full min-w-0">
             <CardContent className="flex flex-col gap-1">
                 <div className="flex items-center justify-between">
-                    <span className="font-medium">{symbol}</span>
+                    <span className="font-medium">{title || symbol}</span>
                     <span className={isPositive ? 'text-gain' : 'text-destructive'}>
                         {isPositive ? '▲' : '▼'} {Math.abs(pct_change).toFixed(2)}%
                     </span>
