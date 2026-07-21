292e47e4f241c39d09124a556a99852b62427352
emmatey <mcrib40@gmail.com>
2026-07-20 21:42:06 -0700

    screeners now selects the tab of the initially chosen screener

--- frontend/src/components/home/shards/Screeners/ScreenersShard.jsx
+++ frontend/src/components/home/shards/Screeners/ScreenersShard.jsx
@@ -73,7 +73,7 @@ export default function ScreenersShard() {
                 </Button>
             </div>
             {screenersSelected.length > 0 ? (
-                <Tabs>
+                <Tabs defaultValue={screenersSelected[0]}>
                     <TabsList>
                         {
                             screenersSelected.map((screener) => {
